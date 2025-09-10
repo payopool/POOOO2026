e4c045371d7a09289989c17ec2eb6989b6651a51
Payoppol <payosf05@gmail.com>
2025-09-10 11:25:01 -0600

    Add Estudiante class and integrate into main
    Introduced a new Estudiante class in estudiantes.h to represent students with name and age. Updated main.cpp to create and initialize an array of Estudiante objects, and added estudiantes.h to the project files and filters.

--- prueba 2/prueba 2/estudiantes.h
+++ prueba 2/prueba 2/estudiantes.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Header.h"
+class Estudiante {
+public:
+    Estudiante() = default;
+    ~Estudiante() = default;
+ 
+    void esturiante(const std::string& name, int edad) {
+        m_name = name;
+        m_edad = edad;
+    
+    }
+
+private:
+    std::string m_name;
+    int m_edad;
+};
--- prueba 2/prueba 2/main.cpp
+++ prueba 2/prueba 2/main.cpp
@@ -3,6 +3,7 @@
 #include"inventory.h"
 #include"rectangulo.h"
 #include"circulo.h"
+#include"estudiantes.h"
 
 
 class
@@ -24,7 +25,21 @@ int main() {
 		std::cout << "radio del circulo:" << circulo.getm_radio() << "\n";
 		std::cout << "area:" << circulo.m_area() << "\n";
 		std::cout << "perimetro:" << circulo.m_preimetro() << "\n";
-	
+		Estudiante estudiante [5];
+		estudiante[0].esturiante("juan", 19);
+		estudiante[1].esturiante("pedro", 20);
+		estudiante[2].esturiante("lupe", 20);
+		estudiante[3].esturiante("ana", 20);
+		estudiante[4].esturiante("pablo", 20);
+		for(int i=0;i<5;i++){
+			std::cout<<"estudiante "<<i+1<< estudiante<<"\n";
+		}
+
+		
+
+
+		
+
 			
 		
 		return 0;
