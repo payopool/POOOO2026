a3c15527daea2171ae1db9741883511901ef46ce
Payoppol <payosf05@gmail.com>
2025-11-11 10:35:04 -0600

    new
    los cambie a igual ._.

--- POO2026/prueba 2/include/programingPa/Bringe/Dispositivo.h
+++ POO2026/prueba 2/include/programingPa/Bringe/Dispositivo.h
@@ -3,11 +3,10 @@
 class Dispositivo {
 public:
 	~Dispositivo() = default;
-	virtual void ecender() {
-		std::cout << "Dispositivo encendido\n";
+	virtual void ecender() = 0;
 
-	}
-	virtual void apagar() {
-		std::cout << "Dispositivo apagado\n";
-	}
+	
+	virtual void apagar() = 0;
+		
+	
 };
